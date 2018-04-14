a8ba3413d009c9d30941b0fd41ae4a48250e4c71
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-04-14 14:03:58 +0200

    Always install latest binaries in Travis-CI

--- ci/install-cargo-make.sh
+++ ci/install-cargo-make.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
-function _installed() {
-    VERSION=$($@ --version 2>/dev/null)
-    [ -z $? ] && echo $VERSION || echo "none"
+_installed() {
+    VERSION=$($@ --version 2>/dev/null || echo "cargo-make none")
+    echo $VERSION | cut -d" " -f2
 }
 
-function _latest() {
-    VERSION=$(cargo search -q $@ | grep $@ | cut -f2 -d"\"")
+_latest() {
+    VERSION=$(cargo search -q "$@" | grep "$@" | cut -f2 -d"\"")
     echo $VERSION
 }
 
 echo -n "Fetching latest available 'cargo-make' version... "
-INSTALLED=$(_installed cargo-make)
+INSTALLED=$(_installed cargo make)
 LATEST=$(_latest cargo-make)
 echo "${LATEST} (installed: ${INSTALLED})"
 
-if [ $INSTALLED == $LATEST ]; then
-  echo "Using latest `cargo-make` (${INSTALLED})"
+if [ "$INSTALLED" == "$LATEST" ]; then
+  echo "Using cached 'cargo-make'"
 else
-  echo "Installing latest `cargo-make` (${INSTALLED})"
-  URL="https://bintray.com/sagiegurari/cargo-make/download_file?file_path=cargo-make_v${LATEST}"
-  curl -SsL $URL > $HOME/.cargo/bin
+  # echo "Installing 'cargo-make' ${LATEST}"
+  # URL="https://bintray.com/sagiegurari/cargo-make/download_file?file_path=cargo-make_v${LATEST}"
+  # curl -SsL $URL > $HOME/.cargo/bin/cargo-make
+  cargo install -f --debug cargo-make
 fi
