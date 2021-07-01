5fbfeaf89f320dd72c8dd999c197e0a0d530626c
Martin Larralde <martin.larralde@embl.de>
2021-07-01 18:14:59 +0200

    Update fuzzer to latest `afl` version and add more examples

--- afl/src/main.rs
+++ afl/src/main.rs
@@ -1,10 +1,13 @@
+#[macro_use]
 extern crate afl;
 extern crate packageurl;
 
 use std::str::FromStr;
 
 fn main() {
-    afl::read_stdio_string(|string| {
-        let _ = packageurl::PackageUrl::from_str(&string);
-    })
+    fuzz!(|data: &[u8]| {
+        if let Ok(s) = std::str::from_utf8(data) {
+            let _ = packageurl::PackageUrl::from_str(&s);
+        }
+    });
 }
