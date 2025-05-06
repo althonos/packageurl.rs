99642a821d7181f111c1e25ea18f37561d8080f1
Jens Reimann <ctron@dentrassi.de>
2025-05-06 16:45:00 +0200

    build: fix up benchmark runs

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,34 +1,26 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use std::time::Duration;
+use packageurl::PackageUrl;
+use std::str::FromStr;
 
-pub(crate) mod benches {
-    use criterion::Criterion;
-    use packageurl::PackageUrl;
-    use std::str::FromStr;
-
-    pub fn bench_from_str(b: &mut Criterion) {
-        let raw_purl = "pkg:type/name/space/name@version?k1=v1&k2=v2#sub/path";
-        b.bench_function("from_str", |_| {
+pub fn bench_from_str(c: &mut Criterion) {
+    let raw_purl = "pkg:type/name/space/name@version?k1=v1&k2=v2#sub/path";
+    c.bench_function("from_str", |c| {
+        c.iter(|| {
             let _ = PackageUrl::from_str(raw_purl).unwrap();
-        });
-    }
+        })
+    });
+}
 
-    pub fn bench_from_url(b: &mut Criterion) {
-        let raw_purl = "pkg:type/name/space/name@version?k1=v1&k2=v2#sub/path";
-        b.bench_function("parse", |_| {
+pub fn bench_from_url(c: &mut Criterion) {
+    let raw_purl = "pkg:type/name/space/name@version?k1=v1&k2=v2#sub/path";
+    c.bench_function("parse", |c| {
+        c.iter(|| {
             let _ = url::Url::parse(raw_purl).unwrap();
-        });
-    }
+        })
+    });
 }
 
 criterion_group! {
-  name = benches;
-  // since insertion takes so long, we need to reduce the sample
-  // size and increase the time so that we can get a few iterations
-  // in between db resets.
-  config = Criterion::default()
-    .measurement_time(Duration::from_secs(15))
-    .sample_size(10);
-  targets = benches::bench_from_str, benches::bench_from_url
+  benches, bench_from_str, bench_from_url
 }
 criterion_main!(benches);
